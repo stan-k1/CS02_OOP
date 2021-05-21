a0db95455600bdfa5e1b3d6a5d4e3e752c4ac32d
Stan K <sk2114@outlook.com>
2021-05-21 15:04:15 +0300

    Clock Lab Added

---
+++ AlarmClock/AlarmClock.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clock
+{
+    class AlarmClock : Clock
+    {
+        public int AlarmHour { get; set; }
+        public int AlarmMin { get; set; }
+        public string AlarmMsg { get; set; }
+
+        public AlarmClock(int hour, int min, int alarmHour, int alarmMin, string alarmMsg) : base (hour, min)
+        {
+
+            if (alarmHour <= 24 && alarmMin <= 60)
+            {
+                AlarmHour = alarmHour;
+                AlarmMin = alarmMin;
+                AlarmMsg = alarmMsg;
+            }
+
+            else Console.WriteLine("Could not create alarm clock. Invalid parameters.");
+        }
+
+        public AlarmClock(int hour, int min, int alarmHour, int alarmMin) : base(hour, min)
+        {
+
+            if (alarmHour <= 24 && alarmMin <= 60)
+            {
+                AlarmHour = alarmHour;
+                AlarmMin = alarmMin;
+                AlarmMsg = "Ring!";
+            }
+
+            else Console.WriteLine("Could not create alarm clock. Invalid parameters.");
+        }
+
+        public bool SetAlarm(int hour, int min, string alarmMsg)
+        {
+            if (hour <= 24 && min <= 60)
+            {
+                hour = Hour;
+                min = Min;
+                AlarmMsg = alarmMsg;
+                return true;
+            }
+
+            Console.WriteLine("Alarm could not be set. Invalid parameters.");
+            return false;
+        }
+
+        public bool SetAlarm(int hour, int min)
+        {
+            if (hour <= 24 && min <= 60)
+            {
+                hour = Hour;
+                min = Min;
+                AlarmMsg = "Ring!";
+                return true;
+            }
+
+            Console.WriteLine("Alarm could not be set. Invalid parameters.");
+            return false;
+        }
+    }
+}
---
+++ AlarmClock/Clock.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clock
+{
+    class Clock
+    {
+        public int Hour { get; set; }
+        public int Min { get; set; }
+
+        public Clock() { }
+
+        public Clock (int hour, int min)
+        {
+            if (hour <= 24 && min <= 60)
+            {
+                Hour = hour;
+                Min = min;
+            }
+            else Console.WriteLine("Could not create clock. Invalid parameters.");
+        }
+
+        public bool SetTime(int hour, int min)
+        {
+            if (hour <= 24 && min < 60)
+            {
+                Hour = hour;
+                Min = min;
+                Console.WriteLine("Hour set!");
+                return true;
+            }
+
+            Console.WriteLine("Invalid hour!");
+            return false;
+        }
+
+        public bool SetTime(string hour, string min) 
+        {
+            int intHour = int.Parse(hour);
+            int intMin = int.Parse(min);
+
+            if (intHour <= 24 && intMin < 60)
+            {
+                Hour = intHour;
+                Min = intMin;
+                Console.WriteLine("Hour set!");
+                return true;
+            }
+
+            Console.WriteLine("Invalid hour!");
+            return false;
+        }
+    }
+}
